0dfcf65befc4874d2865222c82039b3a598b3cb9
tulegenovaaida <tulegenovaaida28@gmail.com>
2026-05-19 20:07:02 +0500

    Update UnweightedGraph.java

--- src/UnweightedGraph.java
+++ src/UnweightedGraph.java
@@ -1,1 +1,13 @@
+public class UnweightedGraph<V> extends WeightedGraph<V> {
 
+    public UnweightedGraph() {
+        super(true);
+    }
+    public UnweightedGraph(boolean undirected) {
+        super(undirected);
+    }
+
+    public void addEdge(V source, V dest) {
+        super.addEdge(source, dest, 1.0);
+    }
+}
