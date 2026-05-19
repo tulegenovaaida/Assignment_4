bdea7382d5c03cd418991882ce4e3a0076e1138b
tulegenovaaida <tulegenovaaida28@gmail.com>
2026-05-19 20:05:17 +0500

    Update BreadthFirstSearch.java

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,1 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class BreadthFirstSearch<V> extends Search<V> {
+
+    public BreadthFirstSearch(UnweightedGraph<V> graph, V source) {
+        super(graph, source);
+
+        bfs(graph, this.source);
+    }
+
+    private void bfs(UnweightedGraph<V> graph, Vertex<V> current) {
+
+        marked.add(current);
+
+        Queue<Vertex<V>> queue = new LinkedList<>();
+        queue.add(current);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> v = queue.remove();
+
+            for (Vertex<V> vertex : graph.adjacencyList(v)) {
+                if (!marked.contains(vertex)) {
+                    marked.add(vertex);
+                    edgeTo.put(vertex, v);
+                    queue.add(vertex);
+                }
+            }
+        }
+    }
+}
