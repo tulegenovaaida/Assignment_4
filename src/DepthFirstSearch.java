93d3bc9b1e5261947fc635ee010852508f0d5cbf
tulegenovaaida <tulegenovaaida28@gmail.com>
2026-05-19 20:05:48 +0500

    Update DepthFirstSearch.java

--- src/DepthFirstSearch.java
+++ src/DepthFirstSearch.java
@@ -1,1 +1,20 @@
+public class DepthFirstSearch<V> extends Search<V> {
 
+    public DepthFirstSearch(UnweightedGraph<V> graph, V source) {
+        super(graph, source);
+
+        dfs(graph, this.source);
+    }
+
+    private void dfs(UnweightedGraph<V> graph, Vertex<V> current) {
+        marked.add(current);
+
+        for (Vertex<V> v : graph.adjacencyList(current)) {
+
+            if (!marked.contains(v)) {
+                edgeTo.put(v, current);
+                dfs(graph, v);
+            }
+        }
+    }
+}
